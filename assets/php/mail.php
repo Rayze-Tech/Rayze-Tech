15729f4c4b473466f820ff72eb9e1645663f2143
Dhruv Batra <dhruvbatra2@gmail.com>
2020-09-19 05:51:25 -0400

    MVP

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hridoy1272@gmail.com";
+        $recipient = "rayzetechnologies@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
